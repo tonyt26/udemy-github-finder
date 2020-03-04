08f27a13c87509a0f77b09422ae16ccb0ff73f67
Ting <ating@us60030898f826.am.sony.com>
2020-03-04 14:09:20 -0800

    fixed errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 //WHAT WE WANT TO RENDER, WHERE WE RENDER (note that root id in index.html renders App component)
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
