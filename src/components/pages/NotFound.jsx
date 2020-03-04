e6326ec5bedf329cd68aec0e370c1837e5d85dd0
Ting <ating@us60030898f826.am.sony.com>
2020-03-04 13:02:49 -0800

    final code

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p className='lead'>Page is not found, please try a different link.</p>
+        </div>
+    )
+}
+
+export default NotFound
